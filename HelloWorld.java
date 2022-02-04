5d43f4076748a7c515c9be2ae62ca4e2331781d4
svelagahotmail <svelaga@hotmail.com>
2022-02-04 12:14:43 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld{
    public static void main(String[] args){
          System.out.println("Git Configurations commit push");
+      System.out.println("****************************************");
    }
 }
